009253aaca57c32af0dafaa01fb83443874c1c4f
FortesFortunaAdiuvat <gg@Guss-MacBook-Pro.local>
2020-08-05 09:24:53 -0600

    Practice with pygsheets and writing dataframe to online service

--- app/edgar/edgarReportingScraper.py
+++ app/edgar/edgarReportingScraper.py
@@ -1,4 +1,5 @@
 import sqlite3, requests, datetime, re
+from lxml import html, etree
 from bs4 import BeautifulSoup
 import pandas as pd
 import pygsheets
@@ -20,10 +21,11 @@ headers = {
 }
 
 response = requests.request("GET", url, headers=headers, data = payload)
-
+print(response.status_code)
+print(response.content.decode('utf-8'))
 # print(response.text.encode('utf8'))
 
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content.decode('utf-8'), 'html.parser')
 # print(soup)
 
 filings_table = soup.find('table')
@@ -38,7 +40,7 @@ for filing_line in filings_list:
     filing_line = re.sub(' +',' ',filing_line) #https://stackoverflow.com/questions/37445285/how-to-best-replace-multiple-whitespaces-by-one-in-python/37445349;  https://stackoverflow.com/questions/2077897/substitute-multiple-whitespace-with-single-whitespace-in-python
     filing_line = filing_line.split(' ',6) # https://stackoverflow.com/questions/30636248/split-a-string-only-by-first-space-in-python/30636260    
     # print(filing_line)
-    if len(filing_line) == 7 and filing_line[0] == '08-03-2020':
+    if len(filing_line) == 7 and filing_line[0] == '08-04-2020':
         print(filing_line[6]+': sec.gov'+filing_line[2].replace('href="','').replace('">SC',''))
         company_name_list.append(filing_line[6])
         filing_detail_link = filing_line[2].replace('href="','').replace('">SC','')
@@ -49,4 +51,32 @@ filing_df['Company Name'] = company_name_list
 filing_df['Filing Detail Link'] = company_filing_detail_link_list
 print(filing_df)
 
-    
+#Full XPath: /html/body/div[4]/div[2]/div/table/tbody/tr[2]/td[3]/a
+#XPath: //*[@id="formDiv"]/div/table/tbody/tr[2]/td[3]/a
+filing_form_link_list = []
+filing_df2 = pd.DataFrame(columns=['Company Name', 'Filing Form Link'])
+for link in filing_df['Filing Detail Link']:
+    # print('sec.gov'+link)
+    full_link = 'https://sec.gov'+link
+    #print(full_link)
+    res = requests.get(url=full_link, headers=headers)
+    #print(res.status_code)
+    # print(response.content.decode('utf-8'))
+    tree = html.fromstring(res.content.decode('utf-8'))
+    root = etree.tostring(tree).decode('utf-8')
+    soup2 = BeautifulSoup(root, 'html.parser')
+    table2 = soup2.find('table')
+    # print(table2)
+    link_set2 = table2.find_all('a', href=True)
+    #print(link_set2[0]['href'])
+    filing_form_link_list.append('https://sec.gov'+link_set2[0]['href'])
+
+filing_df2['Company Name'] = company_name_list
+filing_df2['Filing Form Link'] = filing_form_link_list
+print(filing_df2)
+
+gc = pygsheets.authorize() # must have client_secret.json
+sh = gc.open('TimeMachine_1')
+wks = sh.sheet1
+#wks.update_value('B5', 'test')
+wks.set_dataframe(filing_df2, (1,1))
